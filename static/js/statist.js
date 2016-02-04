12387f16528d403ca0c7efe6092635273abc17e0
liuxiangrong <liuxiangrong@youmi.net>
2016-02-04 17:50:43 +0800

    asd

---
+++ static/js/statist.js
@@ -0,0 +1,11 @@
+define(function() {
+	var _hmt = _hmt || [];
+	(function () {
+	    var hm = document.createElement("script");
+	    hm.src = "//hm.baidu.com/hm.js?4b93dfaedb3bbc5474c9ff73f0ac9b70";
+	    var s = document.getElementsByTagName("script")[0];
+	    s.parentNode.insertBefore(hm, s);
+	})();
+	return _hmt;
+});
+
