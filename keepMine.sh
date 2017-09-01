53a2b7199ef1500ce12f7a1c932931b301bbe0a1
刘祥荣 <xiangrong.liu@archermind.com>
2017-09-01 15:00:55 +0800

    fixed

--- keepMine.sh
+++ keepMine.sh
@@ -1,10 +1,16 @@
+echo [Parent Branch Content]
+echo 
 echo $1
-
+echo
 echo -------------
+echo [This Branch Content] <- Keep This
+echo
 echo $2
+echo
 echo -------------
-
+echo[Remote Branch Content]
+echo
 echo $3
-
+echo
 exit 0
 
